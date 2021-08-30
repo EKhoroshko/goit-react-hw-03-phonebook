61d7c0a4db8baae8271138e5178495d4603e5ba1
EKhoroshko <xapoxa14@gmail.com>
2021-08-30 20:54:43 +0300

    work

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import { Component } from 'react';
+import './App.css';
+import FormSubmit from './components/FormSubmit/FormSubmit';
+import CreateContactList from './components/PhoneList/PhoneList';
+import Filter from './components/Filter/Filter';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    if (JSON.parse(localStorage.getItem('contacts'))) {
+      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+    }
+  }
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  addContact = contact => {
+    if (!this.checkContact(contact.name)) {
+      this.setState(({ contacts }) => ({
+        contacts: [contact, ...contacts],
+      }));
+    } else {
+      alert(`${contact.name} is alredy in contacts`);
+    }
+  };
+
+  checkContact = name => {
+    const { contacts } = this.state;
+    return contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase(),
+    );
+  };
+
+  filterContact = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  removeContact = id => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">Phonebook</h1>
+        <FormSubmit
+          title={'Name'}
+          phone={'Number'}
+          addContact={this.addContact}
+        />
+        <Filter filter={filter} onChange={this.handleChange} />
+        <CreateContactList
+          filterContact={this.filterContact}
+          title={'Contacts'}
+          removeContact={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
